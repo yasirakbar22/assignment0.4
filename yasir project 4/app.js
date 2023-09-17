78e9e3d34bc8bbacfceb5257a33892358cb42462
yasirakbar22 <144627154+yasirakbar22@users.noreply.github.com>
2023-09-18 02:28:31 +0500

    Add files via upload

---
+++ yasir project 4/app.js
@@ -0,0 +1,143 @@
+// (Q1)
+
+// for (var i = 1; i <= 10; i++) {
+//     console.log("Iteration " + i);
+// }
+
+
+// (Q2)
+
+// for (var i = 0; i <= 11; i++) {
+//     console.log("Iteration " + (i + 1));
+// }
+
+
+// (Q3)
+
+// for (var i = 0; i <= 4; i++) {
+//     console.log(i);
+// }
+
+
+// (Q4)
+
+// for (var j = 0; j < 101; j++) {
+//     document.write(j + "<br>");
+// }
+
+
+// (Q5)
+
+// for (var i = 3; i > 0; i--) {
+//     document.write(i + "<br>");
+// }
+
+
+// (Q6)
+
+// var myArray = [1, 2, 3, 4, 5];
+// var arrayLength = myArray.length;
+// document.write("The number of elements in the array is: " + arrayLength);
+
+
+// // (Q7)
+
+// var flag = true; 
+// document.write("The initial value of 'flag' is: " + flag);
+
+
+// (Q8)
+
+// var pets = ["dog", "cat", "fish", "bird", "rabbit"];
+// for (var i = 0; i < pets.length; i++) {
+//     document.write("Iteration" + (i + 1) + ": " + pets[i] + "<br>");
+// }
+
+// (Q9)
+
+// for (var i = 0; i < 10; i++) {
+//     if (i === 1) {
+//         alert("Counter on the second iteration is: " + i);
+//         break;
+//     }
+// }
+
+// console.log("Loop exited after the second iteration.");
+
+// (10)
+
+// var userNames = ["John", "Alice", "Bob", "Eve"]; 
+// var firstName = prompt("Enter first name");
+
+// for (var i = 0; i < userNames.length; i++) {
+//     if (firstName === userNames[i]) {
+//         document.write("Welcome, " + firstName + "!");
+//         break;
+//     } else if (i === userNames.length - 1) {
+//         document.write("Please write the correct user name.");
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
